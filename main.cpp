1aeaf7724a97cbc12343e62fd76836e6fa64732b
Christian Bermejo <cybermejo@gmail.com>
2016-03-09 14:47:24 +0800

    Yet another rewrite
    Added - same window rendering (WIP)
    Cleaned up lots of code
    Added renderers, textures and surfaces
    Much global

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "tinyfiledialogs.h"
 #include <iostream>
 
+#include <sstream>
+
 // For imgui
 #include <SDL2/SDL_opengl.h>
 #include "imgui.h"
@@ -13,153 +15,269 @@
 
 #pragma warning(disable:4996) /* allows usage of strncpy, strcpy, st-rcat, sprintf, fopen */
 
+//Screen dimension constants
 const int SCREEN_WIDTH = 300;
 const int SCREEN_HEIGHT = 120;
+
+//Total windows
+const int TOTAL_WINDOWS = 3;
+
 const char* inImage = "Original";
 const char* outImage = "Anti-aliased";
 
-void loadWindow(std::string path, const char* name, SDL_Window* windowToLoad, SDL_Renderer* rendererToUse, SDL_Surface* surfaceToUse) {
-    windowToLoad = SDL_CreateWindow(
-                                    name,
-                                    SDL_WINDOWPOS_CENTERED,
-                                    SDL_WINDOWPOS_CENTERED,
-                                    640,
-                                    480,
-                                    SDL_WINDOW_SHOWN);
-    if (windowToLoad == NULL) printf("Window could not be created! Error: %s\n", SDL_GetError());
+    // prepare windows, renderers, surfaces
+SDL_Window* mainWindow = NULL;
+SDL_Window* inWindow = NULL;
 
-    rendererToUse = SDL_CreateRenderer(windowToLoad, -1, SDL_RENDERER_ACCELERATED);
-    if (rendererToUse == NULL) printf("Renderer could not be created! Error: %s\n", SDL_GetError());
+SDL_Renderer* inRenderer = NULL;
 
-    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
-    if (loadedSurface == NULL) {
-        printf("Unable to load image %s! Error: %s\n", path.c_str(), SDL_GetError());
-    } else {
-        surfaceToUse = SDL_GetWindowSurface(windowToLoad);
-        SDL_BlitSurface(loadedSurface,NULL, surfaceToUse, NULL);
-        SDL_UpdateWindowSurface(windowToLoad);
+SDL_Surface* inSurface = NULL;
+SDL_Surface* outSurface = NULL;
 
-        SDL_FreeSurface(loadedSurface);
-    }
+SDL_Texture* inTexture = NULL;
+SDL_Texture* outTexture = NULL;
+
+SDL_Rect origPic; //maybe make global?
+SDL_Rect aaPic;
+
+
+int mainWindowID;
+int inWindowID;
+
+int inScreenW;
+int inScreenH;
+
+void close()
+{
+	//Destroy windows
+	for( int i = 0; i < TOTAL_WINDOWS; ++i )
+	{
+//		gWindows[ i ].free();
+	}
+	//Quit SDL subsystems
+
+    SDL_DestroyRenderer(inRenderer);
+    SDL_DestroyWindow(inWindow);
+
+    inRenderer = NULL;
+    inWindow = NULL;
+
+//    ImGui_ImplSdl_Shutdown();
+//    SDL_GL_DeleteContext(glContext);
+//    SDL_DestroyWindow(mainWindow);
+//	SDL_Quit();
 }
 
 
-int main(int argc, char* args[])
+void handleEvent( SDL_Event& e, SDL_Window* mWindow, int mainWindowID)
 {
-    // tinyfiledialog picker
+	//If an event was detected for this window
+	if( e.type == SDL_WINDOWEVENT && e.window.windowID == mainWindowID )
+	{
+		switch( e.window.event )
+		{
+//			//Window appeared
+//			case SDL_WINDOWEVENT_SHOWN:
+//			mShown = true;
+//			break;
+//
+//			//Window disappeared
+//			case SDL_WINDOWEVENT_HIDDEN:
+//			mShown = false;
+//			break;
+//
+//			//Get new dimensions and repaint
+//			case SDL_WINDOWEVENT_SIZE_CHANGED:
+//			mWidth = e.window.data1;
+//			mHeight = e.window.data2;
+//			SDL_RenderPresent( mRenderer );
+//			break;
+
+//			//Repaint on expose
+//			case SDL_WINDOWEVENT_EXPOSED:
+//			SDL_RenderPresent( mRenderer );
+//			break;
+
+			//Hide on close
+			case SDL_WINDOWEVENT_CLOSE:
+			SDL_DestroyWindow( mWindow );
+			break;
+		}
+
+	}
+}
+
+void render(SDL_Renderer* renderer, SDL_Texture* intexture, SDL_Texture* outtexture){
+        //Clear screen
+		SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0x00, 0x00 );
+		SDL_RenderClear( renderer );
+		//Update screen
+		SDL_RenderCopy( renderer, intexture, NULL, &origPic);
+		SDL_RenderCopy( renderer, outtexture, NULL, &aaPic);
+		SDL_RenderPresent( renderer );
+}
+
+void loadWindow(SDL_Surface* insurface, SDL_Surface* outsurface, int surfaceWidth, int surfaceHeight) {
+    //Setup original picture and anti-aliased picture
+//    SDL_Rect origPic; //maybe make global?
+    origPic.w = surfaceWidth;
+    origPic.h = surfaceHeight;
+    origPic.x = 0;
+    origPic.y = 0;
+
+//    SDL_Rect aaPic; //maybe make global?
+    aaPic.w = surfaceWidth;
+    aaPic.h = surfaceHeight;
+    aaPic.x = surfaceWidth;
+    aaPic.y = 0;
+
+    inWindow = SDL_CreateWindow(
+        "Supersampling Anti-aliasing",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1000,
+        500,
+        SDL_WINDOW_SHOWN);
+
+    inRenderer = SDL_CreateRenderer(inWindow, -1, SDL_RENDERER_ACCELERATED);
+
+    inTexture = SDL_CreateTextureFromSurface(inRenderer, insurface);
+    outTexture = SDL_CreateTextureFromSurface(inRenderer, outsurface);
+
+    render(inRenderer, inTexture, outTexture);
+
+}
+
+int main( int argc, char* args[] )
+{
     char const * tfdOpen;
+    SDL_GLContext glContext;
 
-    // prepare windows, renderers, surfaces
-    SDL_Window* mainWindow = NULL;
-    SDL_Window* inWindow = NULL;
-    SDL_Window* outWindow = NULL;
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+        //setup openGL stuff for imgui
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
-    SDL_Renderer* mainRenderer = NULL; //unused?
-    SDL_Renderer* inRenderer = NULL;
-    SDL_Renderer* outRenderer = NULL;
+		 mainWindow = SDL_CreateWindow(
+            "CE101 Project (Bermejo, Corpuz, Imbao)",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_WINDOW_OPENGL
+           );
 
-    SDL_Surface* mainSurface = NULL; //unused?
-    SDL_Surface* inSurface = NULL;
-    SDL_Surface* outSurface = NULL;
+        mainWindowID = SDL_GetWindowID( mainWindow );
 
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0) printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        glContext = SDL_GL_CreateContext(mainWindow);
 
-    //setup openGL stuff for imgui
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
-    mainWindow = SDL_CreateWindow(
-     "CE101 Project (Bermejo, Corpuz, Imbao)",
-     SDL_WINDOWPOS_CENTERED,
-     SDL_WINDOWPOS_CENTERED,
-     SCREEN_WIDTH,
-     SCREEN_HEIGHT,
-     SDL_WINDOW_OPENGL
-     // SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE
-     // SDL_WINDOW_SHOWN
-    );
-
-    SDL_GLContext glContext = SDL_GL_CreateContext(mainWindow);
-
-    //Setup imgui binding
-    ImGuiIO& io = ImGui::GetIO();
-        io.DisplaySize.x = 460.0f;
-        io.DisplaySize.y = 680.0f;
+        //Setup imgui binding
+        ImGuiIO& io = ImGui::GetIO();
+        io.DisplaySize.x = 300.0f;
+        io.DisplaySize.y = 120.0f;
         io.IniFilename = "antialias.ini";
         io.DeltaTime = (1.0f/60.0f);
 
-    ImGui_ImplSdl_Init(mainWindow);
+        ImGui_ImplSdl_Init(mainWindow);
 
-    ImVec4 bgColor = ImColor(114,144,154);
-    bool showmainWindow = false;
-    // Main Loop
-    bool done = false;
-    while (!done)
-    {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            ImGui_ImplSdl_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-        }
+        ImVec4 bgColor = ImColor(114,144,154);
+        bool showmainWindow = true;
+
+		//Main loop flag
+		bool quit = false;
+
+		//Event handler
+		SDL_Event e;
+
+		//While application is running
+		while( !quit )
+		{
+			//Handle events on queue
+			while( SDL_PollEvent( &e ) > 0)
+			{
+			    handleEvent(e, mainWindow, mainWindowID);
+			    handleEvent(e, inWindow, inWindowID);
+
+			    ImGui_ImplSdl_ProcessEvent(&e);
+				//User requests quit
+				if( e.type == SDL_QUIT )
+				{
+					quit = true;
+				}
         ImGui_ImplSdl_NewFrame(mainWindow);
 
         // Main UI
         {
             static float bg_alpha = -0.01f;
             static int i = 0;
-            ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse;
+            ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse;
 
             ImGui::Begin("Supersampling Anti-aliasing", &showmainWindow, ImVec2(294,112), bg_alpha, window_flags);
             if (ImGui::Button("Load Image")) {
+            // THIS IS WHERE THE LOAD IMAGE BUTTON HAPPENS
                 tfdOpen = tinyfd_openFileDialog("Load Image File", "", 0,NULL, NULL, 0);
                 printf("%s\n",tfdOpen);
-                loadWindow(tfdOpen, inImage, inWindow, inRenderer, inSurface);
-                //TODO: Create image loading function here
+
+                inSurface = SDL_LoadBMP(tfdOpen);
+                if (inSurface == NULL) {
+                        printf("Unable to load image %s! Error: %s\n", tfdOpen, SDL_GetError());
+                } else {
+                    inScreenW = inSurface->w;
+                    inScreenH = inSurface->h;
+
+                    loadWindow(inSurface, NULL, inScreenW, inScreenH);
+                    inWindowID = SDL_GetWindowID( inWindow );
+                                 // Add loading function here
+                }
             }
             ImGui::Text("Anti-aliasing Factor");
             ImGui::SliderInt("", &i, 0, 10);
             if (ImGui::Button("Apply")) {
+            // THIS IS WHERE THE APPLY BUTTON HAPPENS
                 //TODO: Add loading image here
+                // POSSIBILITY OF ERROR
+                outSurface = SDL_LoadBMP(tfdOpen);
+                SDL_DestroyWindow(inWindow);
+                inWindow = NULL;
+                loadWindow(inSurface, outSurface, inScreenW, inScreenH);
+                inWindowID = SDL_GetWindowID( inWindow );
             }
             ImGui::End();
         }
-
         // Render imgui to window
         glViewport(0,0,(int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
         glClearColor(bgColor.x, bgColor.y, bgColor.z, bgColor.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui::Render();
         SDL_GL_SwapWindow(mainWindow);
-    }
+			}
+		}
 
-    // Cleanupgithub
-    // close();
-    SDL_DestroyRenderer(inRenderer);
-    SDL_DestroyRenderer(outRenderer);
-    SDL_DestroyWindow(inWindow);
-    SDL_DestroyWindow(outWindow);
+	}
 
-    inRenderer = NULL;
-    outRenderer = NULL;
-    inWindow = NULL;
-    outWindow = NULL;
-
-    ImGui_ImplSdl_Shutdown();
+	//Free resources and close SDL
+	close();
+	ImGui_ImplSdl_Shutdown();
     SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(mainWindow);
-    SDL_Quit();
+	SDL_Quit();
 
-    return 0;
+	return 0;
 }
-
-
-
-
 
 #pragma warning(default:4996)
 
